88e13c3db65d43c0d8a6a75d72e889b11e736557
Scott M Parrish <anithri@gmail.com>
2019-09-12 16:57:26 -0600

    work on base template

---
+++ packages/react-scripts/template/src/pages/index.js
@@ -0,0 +1,29 @@
+import { Route, Router, Switch } from 'react-router-dom';
+import Error404Page from 'pages/Error404';
+import HomePage from 'pages/home';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { createBrowserHistory } from 'history';
+
+export const history = createBrowserHistory();
+
+/* new imports above here - see templates/jg/pages */
+
+const SiteRouter = props => {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route exact path={['/', '/home']} component={HomePage} />
+
+        {/* page routes above here - see _templates/pages/new */}
+        <Route component={Error404Page} />
+      </Switch>
+    </Router>
+  );
+};
+
+SiteRouter.propTypes = {
+  className: PropTypes.string,
+};
+
+export default SiteRouter;
